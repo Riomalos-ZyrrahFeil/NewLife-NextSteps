b7ff5419ca7ca5779cf6462356d4bfba2c44b999
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2025-12-09 02:02:21 +0800

    chore(backend-setup): Update base Controller to extend BaseController and use AuthorizesRequests, ValidatesRequests

--- NextSteps/app/Http/Controllers/Controller.php
+++ NextSteps/app/Http/Controllers/Controller.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
-{
-    //
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{   
+    // 1. AuthorizesRequests: Allows you to use the $this->authorize() method 
+    //    for checking user permissions (e.g., $this->authorize('delete', $user)).
+    use AuthorizesRequests; 
+
+    // 2. ValidatesRequests: Allows you to use the $this->validate() method 
+    //    for easy validation of incoming HTTP requests.
+    use ValidatesRequests;
 }
